ea4ca61423e8e4c764c65a9bce6d5f04d55afd3f
mohamed <mohamedaprahym@gmail.com>
2024-07-03 20:25:24 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,31 +1,23 @@
 #make changes to our configuration file. Just as in the previous configuration file task, we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
 include stdlib
 
-file { '/home/ubuntu/.ssh':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
-
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => 'file',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  require => File['/home/ubuntu/.ssh'],
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^IdentityFile',
-  require => File['/home/ubuntu/.ssh/config'],
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^(\s*)IdentityFile',
+  ensure => present,
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
-  require => File['/home/ubuntu/.ssh/config'],
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^(\s*)PasswordAuthentication',
+  ensure => present,
 }
