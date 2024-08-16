984356eab25a606741ad8c0066039e536cd9a1ee
mohamed <mohamedaprahym@gmail.com>
2024-08-16 05:41:49 +0300

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,14 +8,13 @@ import requests
 
 def recurse(subreddit, hot_list=[], after="", count=0):
     """
-    Fetches and returns a list of titles of all hot posts for a
-    given subreddit.
+    Fetches and returns a list of titles of all hot posts for a given subreddit.
     If the subreddit does not exist or an error occurs, it returns None.
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+            v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
